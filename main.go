cfac350ca9938f90f99cf9c919cac3b2b48fbf6e
zhangyu <zyu0211@gmail.com>
2024-04-29 22:39:30 +0800

    feat: added Route Group Control function

--- main.go
+++ main.go
@@ -9,30 +9,33 @@ import (
 func main() {
 
     engine := yu.New()
-
-    engine.GET("/", func(c *yu.Context) {
-        c.HTML(http.StatusOK, "<h1>Hello yu!</h1>")
+    engine.GET("/index", func(c *yu.Context) {
+        c.HTML(http.StatusOK, "<h1>Index Page</h1>")
     })
 
-    engine.GET("/hello", func(c *yu.Context) {
-        c.String(http.StatusOK, "hello %s, you're at %s\n", c.Query("name"), c.Path)
-    })
-
-    engine.GET("/hello/:name", func(c *yu.Context) {
-		// expect /hello/geektutu
-		c.String(http.StatusOK, "hello %s, you're at %s\n", c.Param("name"), c.Path)
-	})
-
-	engine.GET("/assets/*filepath", func(c *yu.Context) {
-		c.JSON(http.StatusOK, yu.H{"filepath": c.Param("filepath")})
-	})
-
-    engine.POST("/login", func(c *yu.Context) {
-        c.JSON(http.StatusOK, yu.H{
-            "username": c.PostForm("username"),
-            "password": c.PostForm("password"),
+    v1 := engine.Group("/v1")
+    {
+        v1.GET("/", func(c *yu.Context) {
+            c.HTML(http.StatusOK, "<h1>Hello yu</h1>")
         })
-    })
+
+        v1.GET("/hello", func(c *yu.Context) {
+            c.String(http.StatusOK, "hello %s, you're at %s\n", c.Query("name"), c.Path)
+        })
+    }
+    v2 := engine.Group("/v2")
+    {
+        v2.GET("/hello/:name", func(c *yu.Context) {
+            c.String(http.StatusOK, "hello %s, you're at %s\n", c.Param("name"), c.Path)
+        })
+        v2.POST("/login", func(c *yu.Context) {
+            c.JSON(http.StatusOK, yu.H{
+                "username": c.PostForm("username"),
+                "password": c.PostForm("password"),
+            })
+        })
+
+    }
 
     engine.Run(":9999")
 }
--- yu/yu.go
+++ yu/yu.go
@@ -1,20 +1,37 @@
 package yu
 
 import (
-    "net/http"
+	"log"
+	"net/http"
 )
 
 // 定义 handerFunc 类型，以便复用
 type HandlerFunc func(*Context)
 
-// 定义结构体：Engine，实现 http.Handler 接口的 ServeHTTP 方法
-type Engine struct{
-    router *Router
+type RouterGroup struct {
+    prefix string
+    middleWares []HandlerFunc
+    engine *Engine
 }
 
-// 构造方法
+/*
+    定义结构体：Engine，实现 http.Handler 接口的 ServeHTTP 方法
+*/
+type Engine struct {
+    *RouterGroup
+    router *Router
+    groups []*RouterGroup
+}
+
+/*
+    构造方法
+*/
 func New() *Engine {
-    return &Engine{router: newRouter()}
+
+    engine := &Engine{router: newRouter()}
+    engine.RouterGroup = &RouterGroup{engine: engine}
+    engine.groups = []*RouterGroup{engine.RouterGroup}
+    return engine
 }
 
 func (engine *Engine) addRoute(method string, path string, handler HandlerFunc) {
@@ -36,4 +53,32 @@ func (engine *Engine) Run(addr string) (err error) {
 func (engine *Engine) ServeHTTP(w http.ResponseWriter, req *http.Request) {
     c := newContext(w, req)
     engine.router.handle(c)
+}
+
+/*
+    构造一个RouterGroup
+*/
+func (group *RouterGroup) Group(prefix string) *RouterGroup {
+    engine := group.engine
+    newGroup := &RouterGroup {
+        prefix: prefix,
+        engine: engine,     // 新的Group和原Group共用一个 engine
+    }
+    engine.groups = append(engine.groups, newGroup)
+
+    return newGroup
+}
+
+func (group *RouterGroup) addRoute(method string, comp string, handler HandlerFunc) {
+    path := group.prefix + comp
+    log.Printf("Route %4s - %s", method, path)
+    group.engine.router.addRoute(method, path, handler)
+}
+
+func (group *RouterGroup) GET(path string, handler HandlerFunc) {
+    group.addRoute("GET", path, handler)
+}
+
+func (group *RouterGroup) POST(path string, handler HandlerFunc) {
+    group.addRoute("POST", path, handler)
 }
